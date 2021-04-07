4295bfe70a8ca3a065a7042d36a2a53c235dbbf7
Niko <kenniko@protonmail.com>
2021-04-07 12:23:58 +0700

    Class Components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import HelloWorldBtn from "./components/helloWorldBtn";
 
-function App() {
-  const appName = "Hello World App";
-  const appNumber = "Second";
+class App extends React.Component {
+  render() {
+    const appName = "Hello World App";
+    const appNumber = "Second";
 
-  const title = (
-    <h1 className="app-title">
-      My {appNumber} React App, {appName}
-    </h1>
-  );
+    const title = (
+      <h1 className="app-title">
+        My {appNumber} React App, {appName}
+      </h1>
+    );
 
-  console.log(title); // My Second React app, Hello World App
+    // console.log(title); // My Second React app, Hello World App
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {title}
-        <HelloWorldBtn />
-      </header>
-    </div>
-  );
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {title}
+          <HelloWorldBtn appTitle={title} />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
