ec64d6ab2f4d13f8d59a0e68f0ee7ca997cb4a2c
Niko <kenniko@protonmail.com>
2021-04-07 12:26:06 +0700

    State

--- src/components/helloWorldBtn.js
+++ src/components/helloWorldBtn.js
@@ -1,9 +1,29 @@
 import React from "react";
 
-const HelloWorldBtn = (props) => {
-  const { appTitle } = props;
-  const greeting = `Hello world! Greetings from ${appTitle}`;
-  return <button onClick={() => console.log(greeting)}>Hello World</button>;
-};
+class HelloWorldBtn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { greetings: 0 };
+  }
+
+  newGreeting = (greeting) => {
+    const { greetings } = this.state;
+    const newGreetings = greetings + 1;
+    this.setState({
+      greetings: newGreetings,
+    });
+    console.log(greeting);
+  };
+
+  render() {
+    const { appTitle } = this.props;
+    const greeting = `Hello world! Greetings from ${appTitle}`;
+    return (
+      <button onClick={() => this.newGreeting(greeting)}>
+        Hello World, pressed {this.state.greetings} times
+      </button>
+    );
+  }
+}
 
 export default HelloWorldBtn;
