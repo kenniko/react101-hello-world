27b60eb44842b053bf238b5279aea3b23912402c
Niko <kenniko@protonmail.com>
2021-04-07 12:24:44 +0700

    Props

--- src/components/helloWorldBtn.js
+++ src/components/helloWorldBtn.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const HelloWorldBtn = () => {
-  return (
-    <button onClick={() => console.log("Hello World")}>Hello World</button>
-  );
+const HelloWorldBtn = (props) => {
+  const { appTitle } = props;
+  const greeting = `Hello world! Greetings from ${appTitle}`;
+  return <button onClick={() => console.log(greeting)}>Hello World</button>;
 };
 
 export default HelloWorldBtn;
