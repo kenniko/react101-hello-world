bcfcc7dcafd58d689d1a3990dd71a8fdd85e2c7c
Niko <kenniko@protonmail.com>
2021-04-07 12:18:20 +0700

    jsx

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const appName = "Hello World App";
+  const appNumber = "Second";
+
+  const title = (
+    <h1 className="app-title">
+      My {appNumber} React App, {appName}
+    </h1>
+  );
+
+  console.log(title); // My Second React app, Hello World App
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {title}
         <a
           className="App-link"
           href="https://reactjs.org"
